4f3a38df461cd10b85dee7fe2de76fa9d510009c
Heru Handika <herubiolog@gmail.com>
2021-03-02 15:39:14 -0600

    Detect os version.

--- src/utils.R
+++ src/utils.R
@@ -1,4 +1,14 @@
 save_plot <- function(dir, fname) {
   ggsave(here(dir, fname), width = 7, height = 4)
-  system(paste("Powershell.exe Start ", here(dir, fname)))
+  
+  if (is_windowss()) {
+    system(paste("Powershell.exe Start ", here(dir, fname)))
+  } else {
+    system(paste("open ", here(dir, fname)))
+  }
+
+}
+
+is_windowss <- function() {
+  Sys.info()[['sysname']] == "Windows"
 }
