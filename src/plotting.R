da2a0346f776c2684594b2b01e8759cc0ad0ef68
hhandika <herubiolog@gmail.com>
2022-02-21 13:11:13 -0600

    Add locus summary

--- src/plotting.R
+++ src/plotting.R
@@ -6,7 +6,9 @@ library(dplyr)
 
 data <- readr::read_csv(here("data", "id_map.csv"))
 
-locus_summary <- readr::read_csv(here("data", "taxon_summary.csv"))
+taxon_summary <- readr::read_csv(here("data", "taxon_summary.csv"))
+
+locus_summary <- readr::read_csv(here("data", "locus_summary.csv"))
 
 melted <- data |> tidyr::pivot_longer(!Locus, names_to = "species", values_to = "present")
 
@@ -19,7 +21,7 @@ ggplot(melted, aes(x = Locus, y = species, fill = present)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   theme(legend.position = "none")
 
-locus_summary |>
+taxon_summary |>
   dplyr::mutate(site_mb = site_counts / 1e6) |>
   ggplot(aes(x = locus_counts, y = site_mb, color = locus_counts)) + 
   geom_point(size = 2) +
