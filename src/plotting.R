0bae7b110a587f721c476e25136b7bc87b8b38c0
hhandika <herubiolog@gmail.com>
2022-02-21 12:42:29 -0600

    Add locus summary

--- src/plotting.R
+++ src/plotting.R
@@ -6,14 +6,26 @@ library(dplyr)
 
 data <- readr::read_csv(here("data", "id_map.csv"))
 
-melted <- data |> tidyr::pivot_longer(!Alignments, names_to = "species", values_to = "present")
+locus_summary <- readr::read_csv(here("data", "taxon_summary.csv"))
+
+melted <- data |> tidyr::pivot_longer(!Locus, names_to = "species", values_to = "present")
 
 melted |> dplyr::summarise()
 
-ggplot(melted, aes(x = Alignments, y = species, fill = present)) + 
+ggplot(melted, aes(x = Locus, y = species, fill = present)) + 
   geom_tile(aes(size = 2)) +
   scale_fill_manual(values = c("white", "black")) +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   theme(legend.position = "none")
+
+locus_summary |>
+  dplyr::mutate(site_mb = site_counts / 1e6) |>
+  ggplot(aes(x = locus_counts, y = site_mb, color = locus_counts)) + 
+  geom_point(size = 2) +
+  theme_classic() +
+  scale_color_gradient(low="blue", high="red") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(legend.position = "none") +
+  labs(x = "Locus Counts", y = "Site Counts (Mb)")
        
